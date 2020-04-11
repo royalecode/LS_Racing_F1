66143d036d503ff82c7b41e9428c9254cdb82ffb
Artur Alcoverro Pastó <artur.alcoverro@students.salle.url.edu>
2020-04-12 00:49:12 +0200

    Modulos  y Lectura Piezas/Premios

--- conf_Coche.c
+++ conf_Coche.c
@@ -6,22 +6,17 @@
 
 void leerPiloto(Corredor *piloto){
     char msg[MAX_CHAR];//, error[MAX_CHAR];
-    printf("\nNombre del piloto? ");
+    printf("\nNombre del piloto: ");
     fgets((*piloto).nombre, MAX_CHAR, stdin);
     (*piloto).nombre[strlen((*piloto).nombre-1)] = '\0';
-    printf("Nombre de la escuderia? ");
+    printf("Nombre de la escuderia: ");
     fgets((*piloto).escuderia, MAX_CHAR, stdin);
     (*piloto).escuderia[strlen((*piloto).escuderia-1)] = '\0';
-    strcpy(msg,"Dorsal");
-    (*piloto).dorsal = scanRange(1,99, msg);
-    strcpy(msg,"Reflejos");
-    (*piloto).reflejos = scanRange(0,10,msg);
-    strcpy(msg,"Condicion fisica");
-    (*piloto).cond_fisica = scanRange(0,10,msg);
-    strcpy(msg,"Temperamento");
-    (*piloto).temperamento = scanRange(0,10,msg);
-    strcpy(msg,"Gestion de neumaticos");
-    (*piloto).gestion_neumaticos = scanRange(0,10,msg);
+    (*piloto).dorsal = scanRange(1,99, "Dorsal");
+    (*piloto).reflejos = scanRange(0,10,"Reflejos");
+    (*piloto).cond_fisica = scanRange(0,10,"Condicion fisica");
+    (*piloto).temperamento = scanRange(0,10,"Temperamento");
+    (*piloto).gestion_neumaticos = scanRange(0,10,"Gestion de neumaticos");
     printf("\nCargando configurador...\n");
 }
 
