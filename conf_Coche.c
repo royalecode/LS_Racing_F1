69ddcf8037669ccd0b27a75a696cbcd2e1e51a39
Artur Alcoverro Pastó <artur.alcoverro@students.salle.url.edu>
2020-04-25 17:47:33 +0200

    bug fix conf coche

--- conf_Coche.c
+++ conf_Coche.c
@@ -21,17 +21,12 @@ void leerPiloto(Corredor *piloto){
 }
 
 void mostrarGaraje(){
-    BITMAP* garaje;
-    BITMAP* fondo;
+    ALLEGRO_BITMAP* garaje;
     garaje = al_load_bitmap("../imgs/boxes.png");
-    fondo = al_create_bitmap(1000,1000);
-    al_draw_scaled_bitmap(garaje,0,0,0);
+    al_draw_scaled_bitmap(garaje,0,0,1080,810,0,0,600,600,0);
+    //float x1, float y1, float x2, float y2, float x3, float y3, ALLEGRO_COLOR color));
+    //iangle(100,100,100,100)
     al_flip_display();
-    blit()
-
-    while (true);
-
-
 
 
     al_destroy_bitmap(garaje);
